486eeb2b2dc43ea50ca03deca4df29b7c5bbc73e
Lucky <lucky.rana@xchanging.com>
2017-02-21 21:53:50 +0800

    Initial commit

---
+++ app/src/main/java/com/androidluckyguys/ARActivity.java
@@ -0,0 +1,248 @@
+package com.androidluckyguys;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.opengl.Matrix;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ARActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+
+    final static String TAG = "ARActivity";
+    private SurfaceView surfaceView;
+    private FrameLayout cameraContainerLayout;
+    private AROverlayView arOverlayView;
+    private Camera camera;
+    private ARCamera arCamera;
+    private TextView tvCurrentLocation;
+
+    private SensorManager sensorManager;
+    private final static int REQUEST_CAMERA_PERMISSIONS_CODE = 11;
+    public static final int REQUEST_LOCATION_PERMISSIONS_CODE = 0;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 0;//1000 * 60 * 1; // 1 minute
+
+    private LocationManager locationManager;
+    public  Location location;
+    boolean isGPSEnabled;
+    boolean isNetworkEnabled;
+    boolean locationServiceAvailable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+
+        sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        cameraContainerLayout = (FrameLayout) findViewById(R.id.camera_container_layout);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        tvCurrentLocation = (TextView) findViewById(R.id.tv_current_location);
+        arOverlayView = new AROverlayView(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestLocationPermission();
+        requestCameraPermission();
+        registerSensors();
+        initAROverlayView();
+    }
+
+    @Override
+    public void onPause() {
+        releaseCamera();
+        super.onPause();
+    }
+
+    public void requestCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                this.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSIONS_CODE);
+        } else {
+            initARCameraView();
+        }
+    }
+
+    public void requestLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSIONS_CODE);
+        } else {
+            initLocationService();
+        }
+    }
+
+    public void initAROverlayView() {
+        if (arOverlayView.getParent() != null) {
+            ((ViewGroup) arOverlayView.getParent()).removeView(arOverlayView);
+        }
+        cameraContainerLayout.addView(arOverlayView);
+    }
+
+    public void initARCameraView() {
+        reloadSurfaceView();
+
+        if (arCamera == null) {
+            arCamera = new ARCamera(this, surfaceView);
+        }
+        if (arCamera.getParent() != null) {
+            ((ViewGroup) arCamera.getParent()).removeView(arCamera);
+        }
+        cameraContainerLayout.addView(arCamera);
+        arCamera.setKeepScreenOn(true);
+        initCamera();
+    }
+
+    private void initCamera() {
+        int numCams = Camera.getNumberOfCameras();
+        if(numCams > 0){
+            try{
+                camera = Camera.open();
+                camera.startPreview();
+                arCamera.setCamera(camera);
+            } catch (RuntimeException ex){
+                Toast.makeText(this, "Camera not found", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void reloadSurfaceView() {
+        if (surfaceView.getParent() != null) {
+            ((ViewGroup) surfaceView.getParent()).removeView(surfaceView);
+        }
+
+        cameraContainerLayout.addView(surfaceView);
+    }
+
+    private void releaseCamera() {
+        if(camera != null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            arCamera.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private void registerSensors() {
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            float[] rotationMatrixFromVector = new float[16];
+            float[] projectionMatrix = new float[16];
+            float[] rotatedProjectionMatrix = new float[16];
+
+            SensorManager.getRotationMatrixFromVector(rotationMatrixFromVector, sensorEvent.values);
+
+            if (arCamera != null) {
+                projectionMatrix = arCamera.getProjectionMatrix();
+            }
+
+            Matrix.multiplyMM(rotatedProjectionMatrix, 0, projectionMatrix, 0, rotationMatrixFromVector, 0);
+            this.arOverlayView.updateRotatedProjectionMatrix(rotatedProjectionMatrix);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //do nothing
+    }
+
+    private void initLocationService() {
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
+        }
+
+        try   {
+            this.locationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
+            // Get GPS and network status
+            this.isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            this.isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isNetworkEnabled && !isGPSEnabled)    {
+                // cannot get location
+                this.locationServiceAvailable = false;
+            }
+
+            this.locationServiceAvailable = true;
+
+            if (isNetworkEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                if (locationManager != null)   {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    updateLatestLocation();
+                }
+            }
+
+            if (isGPSEnabled)  {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                if (locationManager != null)  {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    updateLatestLocation();
+                }
+            }
+        } catch (Exception ex)  {
+            Log.e(TAG, ex.getMessage());
+
+        }
+    }
+
+    private void updateLatestLocation() {
+        if (arOverlayView !=null) {
+            arOverlayView.updateCurrentLocation(location);
+            tvCurrentLocation.setText(String.format("lat: %s \nlon: %s \naltitude: %s \n",
+                    location.getLatitude(), location.getLongitude(), location.getAltitude()));
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateLatestLocation();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
---
+++ app/src/main/java/com/androidluckyguys/AROverlayView.java
@@ -0,0 +1,112 @@
+package com.androidluckyguys;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.opengl.Matrix;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import com.androidluckyguys.helper.LocationHelper;
+import com.androidluckyguys.model.ARPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.bottom;
+import static android.R.attr.left;
+import static android.R.attr.right;
+import static android.R.attr.top;
+
+/**
+ * Created by ntdat on 1/13/17.
+ */
+
+public class AROverlayView extends View {
+
+    Context context;
+    private float[] rotatedProjectionMatrix = new float[16];
+    private Location currentLocation;
+    private List<ARPoint> arPoints;
+
+
+    public AROverlayView(Context context) {
+        super(context);
+
+        this.context = context;
+
+        //Demo points
+        arPoints = new ArrayList<ARPoint>() {{
+            add(new ARPoint("KLPAC", 3.1850, 101.6868, 0));
+            add(new ARPoint("Twin Tower", 3.1579, 101.7116, 0));
+        }};
+    }
+
+    public void updateRotatedProjectionMatrix(float[] rotatedProjectionMatrix) {
+        this.rotatedProjectionMatrix = rotatedProjectionMatrix;
+        this.invalidate();
+    }
+
+    public void updateCurrentLocation(Location currentLocation){
+        this.currentLocation = currentLocation;
+        this.invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (currentLocation == null) {
+            return;
+        }
+
+        final int radius = 30;
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        paint.setTextSize(60);
+
+        for (int i = 0; i < arPoints.size(); i ++) {
+            float[] currentLocationInECEF = LocationHelper.WSG84toECEF(currentLocation);
+            float[] pointInECEF = LocationHelper.WSG84toECEF(arPoints.get(i).getLocation());
+            float[] pointInENU = LocationHelper.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
+
+            float[] cameraCoordinateVector = new float[4];
+            Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
+
+            // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
+            // if z > 0, the point will display on the opposite
+            if (cameraCoordinateVector[2] < 0) {
+                float x  = (0.5f + cameraCoordinateVector[0]/cameraCoordinateVector[3]) * canvas.getWidth();
+                float y = (0.5f - cameraCoordinateVector[1]/cameraCoordinateVector[3]) * canvas.getHeight();
+
+                Drawable d = null;
+
+                if(arPoints.get(i).getName().equals("KLPAC"))
+                {
+                    d = ContextCompat.getDrawable(context, R.drawable.klpac);
+                }
+                else
+                {
+                    d = ContextCompat.getDrawable(context, R.drawable.twin_tower);
+                }
+
+                Bitmap myBitmap = ((BitmapDrawable)d).getBitmap();
+
+
+                canvas.drawBitmap(myBitmap, x - (30 * arPoints.get(i).getName().length() / 2), y - 80, paint);
+                //canvas.drawCircle(x, y, radius, paint);
+                canvas.drawText(arPoints.get(i).getName(), x - (30 * arPoints.get(i).getName().length() / 2), y - 80, paint);
+
+
+            }
+        }
+    }
+}
